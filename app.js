d2ed1673b8fa746d66ef9a83fd7f2fb31f321937
paulg44 <paulgarton1984@hotmail.com>
2023-05-12 14:19:24 +0100

    create & edit

--- app.js
+++ app.js
@@ -9,33 +9,51 @@ import {
 } from "./recipes.js";
 
 const app = express();
-const PORT = 3005;
+const PORT = 3000;
 
 // app.use(express.static("public"));
 app.use(express.json());
 
 // plan for getRecipes route handler
 // create a variable and equate it to await getRecipes
-// our response is 
-app.get('/api/recipes', async (req,res) => {
-  console.log("recipes works")
+// our response is
+app.get("/api/recipes", async (req, res) => {
+  console.log("recipes works");
   const recipes = await getRecipes();
-  const response = {success: true, payload: recipes,}
-  res.send(response)
-})
+  const response = { success: true, payload: recipes };
+  res.send(response);
+});
 
 //get recipe by ID route hander:
 // create variable id that is assigned to id value of recipe
 //create a variable for getRecipeById()
 //response (success boolean, payload)
-app.get('/api/recipes/:id', async (req, res) => {
+app.get("/api/recipes/:id", async (req, res) => {
   console.log("get recipe id has worked");
-   const id = req.params.id//assigns id value of url field to variable
-  const recipeById = await getRecipeByID(id)//spread out over two lines to make it more 'readable'
-  res.json(recipeById)
-})
-  
+  const id = req.params.id; //assigns id value of url field to variable
+  const recipeById = await getRecipeByID(id); //spread out over two lines to make it more 'readable'
+  res.json(recipeById);
+});
+
+// Create route handler
+app.post("/api/recipes/", async (req, res) => {
+  console.log("create recipe has worked");
+  const newRecipe = req.body;
+  const createNewRecipe = await createRecipe(newRecipe);
+  res.json(createNewRecipe);
+});
+
+// Update Recipe route handler
+app.patch("/api/recipes/:id", async (req, res) => {
+  console.log("update recipe has worked");
+
+  const id = req.params.id;
+
+  const updatedRecipe = req.body;
+  const updateRecipe = await updateRecipeByID(updatedRecipe, id);
+  res.json(updateRecipe);
+});
 
 app.listen(PORT, () => {
-  console.log (`Server listening on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -9,34 +9,75 @@ const fileName = "recipes.json";
 // parse its content
 // return the whole array
 export async function getRecipes() {
-    console.log("get recipes worked");
-    const allRecipesJSON = await fs.readFile(fileName,"utf-8");
-    const allRecipes = JSON.parse(allRecipesJSON);
-    return allRecipes
+  console.log("get recipes worked");
+  const allRecipesJSON = await fs.readFile(fileName, "utf-8");
+  const allRecipes = JSON.parse(allRecipesJSON);
+  return allRecipes;
 }
 //read recipes and save to variable
 //parse its contents
 //if statement -> if id matches router id then return recipe
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {
-    console.log("get recipe by ID worked");
-    const allRecipesJSON = await fs.readFile(fileName,"utf-8");
-    const allRecipes = JSON.parse(allRecipesJSON);
-    for (let i = 0; i < allRecipes.length; i++){
-        if (allRecipes[i].id === id){
-            console.log('recipe by id', allRecipes[i])
-            return allRecipes[i]
-        }
-    }return null;
-    
+  console.log("get recipe by ID worked");
+  const allRecipesJSON = await fs.readFile(fileName, "utf-8");
+  const allRecipes = JSON.parse(allRecipesJSON);
+  for (let i = 0; i < allRecipes.length; i++) {
+    if (allRecipes[i].id === id) {
+      return allRecipes[i];
+    }
+  }
+  return null;
 }
 
-
+//read recipes and save to variable
+//parse its contents
+// push new recipe to to array
 // CREATE A RECIPE
-export async function createRecipe(newRecipe) {}
+export async function createRecipe(newRecipe) {
+  console.log("create recipe worked");
+  const allRecipesJSON = await fs.readFile(fileName, "utf-8");
+  const allRecipes = JSON.parse(allRecipesJSON);
 
+  const recipe = {
+    id: uuidv4(),
+    // Spread operator used
+    ...newRecipe,
+  };
+
+  allRecipes.push(recipe);
+  await fs.writeFile(fileName, JSON.stringify(allRecipes));
+
+  return recipe;
+}
+
+//read recipes and save to variable
+//parse its contents
+// update recipe by ID
+// Return updated recipe
 // UPDATE A RECIPE BY ID
-export async function updateRecipeByID(id, updatedRecipe) {}
+export async function updateRecipeByID(updatedRecipe, id) {
+  console.log("update recipe ID worked");
+  const allRecipesJSON = await fs.readFile(fileName, "utf-8");
+  const allRecipes = JSON.parse(allRecipesJSON);
+
+  let recipe = null;
+
+  for (let i = 0; i < allRecipes.length; i++) {
+    if (allRecipes[i].id === id) {
+      recipe = allRecipes[i];
+      allRecipes[i].title = updatedRecipe.title;
+      allRecipes[i].ingredients = updatedRecipe.ingredients;
+      allRecipes[i].instructions = updatedRecipe.instructions;
+      allRecipes[i].image = updatedRecipe.image;
+      //   allRecipes[i].title = updatedRecipe.title;
+    }
+  }
+
+  await fs.writeFile(fileName, JSON.stringify(allRecipes));
+
+  return recipe;
+}
 
 // DELETE A RECIPE BY ID
 export async function deleteRecipeByID(id) {}
