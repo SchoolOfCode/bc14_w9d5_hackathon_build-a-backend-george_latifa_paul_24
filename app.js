62e8651520209bbaa8bd77213ba888dfae9f1d08
georgeussher <gussher@icloud.com>
2023-05-12 11:58:39 +0100

    getby id stuck

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ import {
 } from "./recipes.js";
 
 const app = express();
-const PORT = 3000;
+const PORT = 3005;
 
 // app.use(express.static("public"));
 app.use(express.json());
@@ -17,14 +17,23 @@ app.use(express.json());
 // plan for getRecipes route handler
 // create a variable and equate it to await getRecipes
 // our response is 
-
 app.get('/api/recipes', async (req,res) => {
   console.log("recipes works")
   const recipes = await getRecipes();
- const response = {success: true, payload: recipes,}
- res.send(response)
+  const response = {success: true, payload: recipes,}
+  res.send(response)
 })
-  
+
+//get recipe by ID route hander:
+// create variable id that is assigned to id value of recipe
+//create a variable for getRecipeById()
+//response (success boolean, payload)
+app.get('api/recipes/:id', async (req, res) => {
+  console.log("get recipe id has worked");
+   const id = req.params.id//assigns id value of url field to variable
+  const recipeById = await getRecipeById(urlId)//spread out over two lines to make it more 'readable'
+  res.json(recipeById)
+})
   
 
 app.listen(PORT, () => {
--- recipes.js
+++ recipes.js
@@ -14,9 +14,23 @@ export async function getRecipes() {
     const allRecipes = JSON.parse(allRecipesJSON);
     return allRecipes
 }
-
+//read recipes and save to variable
+//parse its contents
+//if statement -> if id matches router id then return recipe
 // GET A RECIPE BY ID
-export async function getRecipeByID(id) {}
+export async function getRecipeByID(id) {
+    console.log("get recipe by ID worked");
+    const allRecipesJSON = await fs.readFile(fileName,"utf-8");
+    const allRecipes = JSON.parse(allRecipesJSON);
+    for (let i = 0; i < allRecipes.length; i++){
+        if (allRecipes[i].id === id){
+            console.log('recipe by id', allRecipes[i])
+            return allRecipes[i]
+        }
+    }
+    return null;
+}
+
 
 // CREATE A RECIPE
 export async function createRecipe(newRecipe) {}
