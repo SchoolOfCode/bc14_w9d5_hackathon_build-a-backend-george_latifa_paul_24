622add9744286a60d0279dc27ae6e64e412dc29c
latifahassan <latifahassanali@outlook.com>
2023-05-12 10:52:04 +0100

    getRecipes

--- app.js
+++ app.js
@@ -11,9 +11,22 @@ import {
 const app = express();
 const PORT = 3000;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 app.use(express.json());
 
+// plan for getRecipes route handler
+// create a variable and equate it to await getRecipes
+// our response is 
+
+app.get('/api/recipes', async (req,res) => {
+  console.log("recipes works")
+  const recipes = await getRecipes();
+ const response = {success: true, payload: recipes,}
+ res.send(response)
+})
+  
+  
+
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log (`Server listening on port ${PORT}`);
 });
--- recipes.js
+++ recipes.js
@@ -4,7 +4,16 @@ import { v4 as uuidv4 } from "uuid";
 const fileName = "recipes.json";
 
 // GET ALL RECIPES
-export async function getRecipes() {}
+
+// read the recipes.json file and save it to the variable
+// parse its content
+// return the whole array
+export async function getRecipes() {
+    console.log("get recipes worked");
+    const allRecipesJSON = await fs.readFile(fileName,"utf-8");
+    const allRecipes = JSON.parse(allRecipesJSON);
+    return allRecipes
+}
 
 // GET A RECIPE BY ID
 export async function getRecipeByID(id) {}
